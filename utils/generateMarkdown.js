16e08252dbba01da2fd058c5d0d68b4c27a58a40
rhazama <rhazama01@gmail.com>
2022-04-26 18:22:09 -0700

    added to readme template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,7 +6,7 @@
 // function renderLicenseSection(license) {}
 
 //creates a function to generate markdown for README
-function generateMarkdown(data) {
+function generateMarkdown(answers) {
     return `
 <h1 align="center">${answers.title} </h1>
 
@@ -18,28 +18,19 @@ ${answers.description}
 - [Installation](#installation)
 - [Usage](#usage)
 - [License](#license)
-- [Credits](#credits)
+- [Contributing](#contributing)
 - [Tests](#tests)
 - [Questions](#questions)
-
 ## Installation
 ${answers.installation}
 ## Usage
 ${answers.usage}
-## Credits
-${answers.credits}
 ## License
-
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
-## Badges
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
+![badge](https://img.shields.io/badge/license-${answers.license}-green)
+<br />
+This application is covered by the ${answers.license} license.
 ## Contributing
 ${answers.contributing}
-
 ## Tests
 ${answers.tests}
 ## Questions
